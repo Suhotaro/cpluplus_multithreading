ffa51a26a24171b6c9c486a3810181db90fcf631
Roman Peresypkin <nevebackdown@yahoo.com>
2016-12-05 23:19:31 +0200

    5.1.1 atomic operations

---
+++ src/5.1.1.atomic_bool/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <atomic>
+#include <cassert>
+
+using namespace std;
+
+void ex1() {
+	atomic<bool> b;
+	cout << "1: b:" << b.load(std::memory_order_acquire) << endl;
+
+	b.store(false);
+	cout << "1: b:" << b.load(std::memory_order_acquire) << endl;
+
+	b.store(true);
+	cout << "1: b:" << b.load(std::memory_order_acquire) << endl;
+
+	bool x = b.exchange(false, std::memory_order_acq_rel);
+	cout << "1: b:" << b.load(std::memory_order_acquire) << ", x:" << x << endl;
+
+	x = b.exchange(false, std::memory_order_acq_rel);
+	cout << "1: b:" << b.load(std::memory_order_acquire) << ", x:" << x << endl;
+
+	x = b.exchange(true, std::memory_order_acq_rel);
+	cout << "1: b:" << b.load(std::memory_order_acquire) << ", x:" << x << endl;
+}
+
+void ex2() {
+	atomic<int> a;
+	int b = 4;
+	int c = 5;
+	bool ex = false;
+	a = 3;
+
+	auto handle = [&]() {
+		cout << "ex: " << ex << ", "
+		     << "a:" << a << ", "
+			 << "b:" << b << ", "
+			 << "c:" << c << ", "
+			 << endl;
+	};
+
+	handle();
+
+	ex = a.compare_exchange_strong(b, c);
+	handle();
+
+	ex = a.compare_exchange_strong(b, c);
+	handle();
+
+	c = 7;
+	ex = a.compare_exchange_strong(b, c);
+	handle();
+
+	ex = a.compare_exchange_strong(b, c);
+	handle();
+}
+
+void ex3() {
+	class A {
+	public:
+		A() {}
+	};
+
+	A arr[5];
+	std::atomic<A*> p(arr);
+
+	A* x = p.fetch_add(2);
+
+	assert(x == arr);
+	assert(p.load() == &arr[2]);
+
+	x = (p-=1);
+	assert(x == &arr[1]);
+	assert(p.load() == &arr[1]);
+}
+
+int main()
+{
+	ex1();
+	ex2();
+	ex3();
+
+	return 0;
+}
